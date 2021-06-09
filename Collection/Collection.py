4bd2adbbdb121a1ca1b3f21c8f193aba4e99c4f9
wioenena-q <brs_kprl09@outlook.com>
2021-06-09 21:26:21 +0300

    Added utility methods.

--- Collection/Collection.py
+++ Collection/Collection.py
@@ -1,15 +1,22 @@
+from random import randint
+
+
 class Collection():
-    def __init__(self, vType):
+    def __init__(self, vType, *collections: tuple):
         self.__vType = vType
         self.__cache = dict()
         self.__size = 0
+        if len(collections) > 0:
+            for collection in collections:
+                for k, v in collection.entries():
+                    self.set(k, v)
 
     def set(self, key: str, value):
         if isinstance(value, self.__vType) is False:
             raise Exception("The value does not match the specified type.")
 
         self.__cache[key] = value
-        return value
+        return self
 
     def get(self, key: str):
         return self.__cache.get(key)
@@ -20,6 +27,7 @@ class Collection():
     def has(self, key: str):
         if self.__cache.get(key) is not None:
             return True
+
         return False
 
     def clear(self) -> 'Collection':
@@ -37,7 +45,12 @@ class Collection():
         return self.__cache.__iter__()
 
     def entries(self):
-        return self.__cache.items()
+        result = []
+
+        for k in self.key_list():
+            result.append([k, self.get(k)])
+
+        return result
 
     def keys(self) -> 'iter':
         return iter(self.__cache.keys())
@@ -45,6 +58,264 @@ class Collection():
     def values(self) -> 'iter':
         return iter(self.__cache.values())
 
-    def for_each(self,fn):
-        for k,v in self.entries():
-            fn(k,v,self)
+    def for_each(self, fn):
+        for k, v in self.entries():
+            fn(v, k, self)
+        return None
+
+    def map(self, fn):
+        result = []
+
+        for k, v in self.entries():
+            result.append(fn(v, k, self))
+        return result
+
+    def filter(self, fn):
+        result = Collection(self.__vType)
+
+        for k, v in self.entries():
+            if fn(v, k, self) == True:
+                result.set(k, v)
+
+        return result
+
+    def some(self, fn) -> bool:
+        for k, v in self.entries():
+            if fn(v, k, self) == True:
+                return True
+
+        return False
+
+    def every(self, fn) -> bool:
+        for k, v in self.entries():
+            if fn(v, k, self) == False:
+                return False
+
+        return True
+
+    def equals(self, collection: 'Collection') -> bool:
+        if collection.get_size() is not self.get_size():
+            return False
+
+        for k, v in collection.entries():
+            if v == self.get(k):
+                return True
+
+        return False
+
+    def clone(self) -> 'Collection':
+        return Collection(self.__vType, self)
+
+    def concat(self, *collections: tuple):
+        if len(collections) == 0:
+            return self
+
+        clone = self.clone()
+
+        for collection in collections:
+            for k, v in collection.entries():
+                clone.set(k, v)
+
+        return clone
+
+    def difference(self, other: 'Collection') -> 'Collection':
+        clone = Collection(self.__vType)
+
+        for k, v in self.entries():
+            if other.has(k) == False:
+                clone.set(k, v)
+                print(k)
+
+        for k, v in other.entries():
+            if self.has(k) == False:
+                clone.set(k, v)
+
+        return clone
+
+    def each(self, fn):
+        self.for_each(fn)
+        return self
+
+    def find_key(self, fn):
+        for k, v in self.entries():
+            if fn(v, k, self) == True:
+                return k
+
+        return None
+
+    def find_value(self, fn):
+        for k, v in self.entries():
+            if fn(v, k, self) == True:
+                return v
+
+        return None
+
+    def first(self, amount=None):
+        _iter = self.values()
+
+        if amount == None:
+            return next(_iter)
+
+        if amount < 0:
+            return self.last(amount * -1)
+
+        amount = min(self.get_size(), amount)
+
+        result = list()
+
+        for n in range(amount):
+            result.append(next(_iter))
+
+        return result
+
+    def first_key(self, amount=None):
+        _iter = self.keys()
+
+        if amount == None:
+            return next(_iter)
+
+        if amount < 0:
+            return self.last_key(amount * -1)
+
+        amount = min(self.get_size(), amount)
+
+        result = list()
+
+        for n in range(amount):
+            result.append(next(_iter))
+
+        return result
+
+    def last(self, amount=None):
+        values = list(self.values())
+
+        if amount == None:
+            return values[len(values) - 1]
+
+        if amount < 0:
+            return self.first(amount * -1)
+
+        amount = min(self.get_size(), amount)
+
+        return values[-amount:]
+
+    def last_key(self, amount=None):
+        keys = list(self.keys())
+
+        if amount == None:
+            return keys[len(keys) - 1]
+
+        if amount < 0:
+            return self.first_key(amount * -1)
+
+        amount = min(self.get_size(), amount)
+
+        return keys[-amount:]
+
+    def to_list(self) -> list:
+        return list(self.values())
+
+    def key_list(self) -> list:
+        return list(self.keys())
+
+    def flat_map(self, fn):
+        result_collection = Collection(self.__vType)
+
+        result = self.map(fn)
+
+        for e in result:
+            if isinstance(e, Collection):
+                for k, v in e.entries():
+                    result_collection.set(k, v)
+
+        return result_collection
+
+    def intersect(self, other: 'Collection') -> 'Collection':
+        def fn(_v, k, _c):
+            return self.has(k)
+
+        return other.filter(fn)
+
+    def map_values(self, fn) -> 'Collection':
+        result = Collection(self.__vType)
+
+        for k, v in self.entries():
+            result.set(k, fn(v, k, self))
+
+        return result
+
+    def partition(self, fn) -> list:
+        result = [
+            Collection(self.__vType),
+            Collection(self.__vType)
+        ]
+
+        for k, v in self.entries():
+            if fn(v, k, self) == True:
+                result[0].set(k, v)
+            else:
+                result[1].set(k, v)
+
+        return result
+
+    def random(self, amount=None):
+        values = self.to_list()
+
+        if amount == None:
+            return values[randint(0, self.get_size() - 1)]
+
+        result = []
+
+        for n in range(amount):
+            result.append(self.random())
+
+        return result
+
+    def random_key(self, amount=None):
+        keys = self.key_list()
+
+        if amount == None:
+            return keys[randint(0, self.get_size() - 1)]
+
+        result = []
+
+        for n in range(amount):
+            result.append(self.random_key())
+
+        return result
+
+    def reduce(self, fn, initalValue=None):
+        accumulator = None
+
+        if initalValue is not None:
+            accumulator = initalValue
+
+            for k, v in self.entries():
+                accumulator = fn(accumulator, v, k, self)
+
+            return accumulator
+
+        first = True
+
+        for k, v in self.entries():
+            if first == True:
+                accumulator = fn(accumulator, v, k, self)
+                first = False
+            else:
+                accumulator = fn(accumulator, v, k, self)
+
+        return accumulator
+
+    def sweep(self, fn) -> int:
+        total = 0
+
+        for k, v in self.entries():
+            if fn(v, k, self) == True:
+                self.delete(k)
+                total += 1
+
+        return total
+
+    def tap(self, fn):
+        fn(self)
+        return self
--- test/test_main.py
+++ test/test_main.py
@@ -7,12 +7,7 @@ v = coll.set("test", 3)
 
 
 def test_set():
-    assert v == 3
-
-
-@pytest.mark.skip
-def test_set_err():
-    assert v == 4
+    assert v == coll
 
 
 def test_get():
@@ -23,18 +18,8 @@ def test_get_none():
     assert coll.get("testNone") == None
 
 
-@pytest.mark.skip
-def test_get_err():
-    assert coll.get("test") == 4
-
-
 def test_get_size():
     assert coll.get_size() == 1
-
-
-@pytest.mark.skip
-def test_get_size_err():
-    assert coll.get_size() == 2
 
 
 def test_has():
@@ -45,18 +30,8 @@ def test_has_with_false():
     assert coll.has("testNone") == False
 
 
-@pytest.mark.skip
-def test_has_err():
-    assert coll.has("testNone") == True
-
-
 def test_clear():
     assert coll.clear().get_size() == 0
-
-
-@pytest.mark.skip
-def test_clear_err():
-    assert coll.clear().get_size() == 1
 
 
 def test_delete():
@@ -68,21 +43,10 @@ def test_delete_with_not_exist():
     assert coll.delete("test") == False
 
 
-@pytest.mark.skip
-def test_delete_err():
-    assert coll.delete("test") == True
-
-
 def test_iter():
     coll.set("test", 3)
     for item in coll:
         assert item == "test"
-
-
-@pytest.mark.skip
-def test_iter_err():
-    for item in coll:
-        assert item == "error"
 
 
 def test_entries():
@@ -91,43 +55,209 @@ def test_entries():
         assert v == 3
 
 
-@pytest.mark.skip
-def test_entries_error():
-    for k, v in coll.entries():
-        assert k == 3
-        assert v == "test"
-
-
 def test_keys():
     assert next(coll.keys()) == "test"
-
-
-@pytest.mark.skip
-def test_keys_err():
-    assert next(coll.keys()) == 3
 
 
 def test_values():
     assert next(coll.values()) == 3
 
 
-@pytest.mark.skip
-def test_values_err():
-    assert next(coll.values()) == "test"
-
-
 def test_for_each():
-    def fn(k, v, c):
+    def fn(v, k, c):
         assert k == "test"
         assert v == 3
         assert c == coll
+
     coll.for_each(fn)
 
 
-@pytest.mark.skip
-def test_for_each_err():
-    def fn(k, v, c):
-        assert k == 3
-        assert v == coll
-        assert c == "test"
-    coll.for_each(fn)
+def test_map():
+    def fn(v, k, c):
+        return v * 2
+
+    assert coll.map(fn) == [6]
+
+
+def test_filter():
+    def fn(v, k, c):
+        return v == 3
+    assert coll.filter(fn).get_size() == coll.get_size()
+
+
+def test_some():
+    def fn(v, k, c):
+        return v == 3 and k == "test"
+    assert coll.some(fn) == True
+
+
+def test_every():
+    coll.set("testSecond", 4)
+
+    def fn(v, k, c):
+        return v == 3
+
+    assert coll.every(fn) == False
+
+
+def test_equal():
+    coll2 = coll.clone()
+
+    assert coll.equals(coll2) == True
+
+
+def test_concat():
+    coll2 = Collection(int)
+    for n in range(10):
+        coll2.set(str(n), n)
+
+    concat = coll.concat(coll2)
+
+    assert concat.get_size() == 12
+
+
+def test_difference():
+    coll2 = Collection(int)
+    coll3 = Collection(int)
+    for n in range(10):
+        coll2.set(str(n), n)
+
+    for n in range(15):
+        coll3.set(str(n), n)
+
+    assert coll2.difference(coll3).get_size() == 5
+
+
+def test_each():
+    def fn(v, k, c):
+        assert c == coll
+
+    coll.each(fn)
+
+
+def test_find_key():
+    def fn(v, k, c):
+        return k == "test"
+
+    assert coll.find_key(fn) == "test"
+
+
+def test_find_value():
+    def fn(v, k, c):
+        return k == "test"
+
+    assert coll.find_value(fn) == 3
+
+
+def test_first_without_amount():
+    assert coll.first() == 3
+
+
+def test_first_with_amount():
+    assert coll.first(5) == [3, 4]
+
+
+def test_first_with_negative_amount():
+    coll.set("added", 5)
+    assert coll.first(-2) == [4, 5]
+
+
+def test_last_without_amount():
+    assert coll.last() == 5
+
+
+def test_last_with_amount():
+    coll.clear()
+    for n in range(10):
+        coll.set(str(n), n)
+
+    assert coll.last(2) == [8, 9]
+
+
+def test_last_with_negative_amount():
+    assert coll.last(-2) == [0, 1]
+
+
+def test_first_key_without_amount():
+    assert coll.first_key() == "0"
+
+
+def test_first_key_with_amount():
+    assert coll.first_key(2) == ["0", "1"]
+
+
+def test_first_key_with_negative_amount():
+    assert coll.first_key(-2) == ["8", "9"]
+
+
+def test_last_key_without_amount():
+    assert coll.last_key() == "9"
+
+
+def test_last_key_with_amount():
+    assert coll.last_key(2) == ["8", "9"]
+
+
+def test_last_key_with_negative_amount():
+    assert coll.last_key(-2) == ["0", "1"]
+
+
+def test_to_list():
+    coll.clear()
+    coll.set("0", 0)
+    coll.set("1", 1)
+
+    assert coll.to_list() == [0, 1]
+
+
+def test_key_list():
+    assert coll.key_list() == ["0", "1"]
+
+
+def test_flat_map():
+    coll2 = Collection(int)
+
+    def fn(v, k, c):
+        return coll2.set(k, v)
+
+    assert coll.flat_map(fn).equals(coll2) == True
+
+
+def test_intersect():
+
+    clone = coll.clone()
+    clone.clear()
+
+    for n in range(2, 5):
+        clone.set(str(n), n)
+
+    coll.clear()
+    for n in range(5):
+        coll.set(str(n), n)
+
+    assert clone.get_size() == 3
+
+
+def test_map_values():
+    result = coll.map_values(lambda v, _, _c: v)
+
+    assert coll.equals(result) == True
+
+
+def test_partition():
+    result = coll.partition(lambda v, _k, _c: v < 3)
+
+    assert result[0].equals(result[1]) == False
+
+
+def test_reduce():
+    coll.clear()
+
+    for n in range(5):
+        coll.set(str(n), n)
+
+    assert coll.reduce(lambda a, v, _k, _c: a + v, 0) == 10
+
+
+def test_sweep():
+    assert coll.sweep(lambda v, _k, _c: v < 10) == 5
