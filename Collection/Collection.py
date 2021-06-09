88f31d27295947523a44011ee81166c343cf633b
wioenena-q <brs_kprl09@outlook.com>
2021-06-09 16:22:39 +0300

    Adapted the main structures in Javascript.

--- Collection/Collection.py
+++ Collection/Collection.py
@@ -2,6 +2,7 @@ class Collection():
     def __init__(self, vType):
         self.__vType = vType
         self.__cache = dict()
+        self.__size = 0
 
     def set(self, key: str, value):
         if isinstance(value, self.__vType) is False:
@@ -11,7 +12,39 @@ class Collection():
         return value
 
     def get(self, key: str):
+        return self.__cache.get(key)
+
+    def get_size(self) -> int:
+        return len(self.__cache)
+
+    def has(self, key: str):
+        if self.__cache.get(key) is not None:
+            return True
+        return False
+
+    def clear(self) -> 'Collection':
+        self.__cache.clear()
+        return self
+
+    def delete(self, key: str):
         try:
-            return self.__cache[key]
+            del self.__cache[key]
+            return True
         except:
-            return None
+            return False
+
+    def __iter__(self):
+        return self.__cache.__iter__()
+
+    def entries(self):
+        return self.__cache.items()
+
+    def keys(self) -> 'iter':
+        return iter(self.__cache.keys())
+
+    def values(self) -> 'iter':
+        return iter(self.__cache.values())
+
+    def for_each(self,fn):
+        for k,v in self.entries():
+            fn(k,v,self)
--- test/test_main.py
+++ test/test_main.py
@@ -12,4 +12,122 @@ def test_set():
 
 @pytest.mark.skip
 def test_set_err():
-    assert v == 4  # Error
+    assert v == 4
+
+
+def test_get():
+    assert coll.get("test") == 3
+
+
+def test_get_none():
+    assert coll.get("testNone") == None
+
+
+@pytest.mark.skip
+def test_get_err():
+    assert coll.get("test") == 4
+
+
+def test_get_size():
+    assert coll.get_size() == 1
+
+
+@pytest.mark.skip
+def test_get_size_err():
+    assert coll.get_size() == 2
+
+
+def test_has():
+    assert coll.has("test") == True
+
+
+def test_has_with_false():
+    assert coll.has("testNone") == False
+
+
+@pytest.mark.skip
+def test_has_err():
+    assert coll.has("testNone") == True
+
+
+def test_clear():
+    assert coll.clear().get_size() == 0
+
+
+@pytest.mark.skip
+def test_clear_err():
+    assert coll.clear().get_size() == 1
+
+
+def test_delete():
+    coll.set("test", 3)
+    assert coll.delete("test") == True
+
+
+def test_delete_with_not_exist():
+    assert coll.delete("test") == False
+
+
+@pytest.mark.skip
+def test_delete_err():
+    assert coll.delete("test") == True
+
+
+def test_iter():
+    coll.set("test", 3)
+    for item in coll:
+        assert item == "test"
+
+
+@pytest.mark.skip
+def test_iter_err():
+    for item in coll:
+        assert item == "error"
+
+
+def test_entries():
+    for k, v in coll.entries():
+        assert k == "test"
+        assert v == 3
+
+
+@pytest.mark.skip
+def test_entries_error():
+    for k, v in coll.entries():
+        assert k == 3
+        assert v == "test"
+
+
+def test_keys():
+    assert next(coll.keys()) == "test"
+
+
+@pytest.mark.skip
+def test_keys_err():
+    assert next(coll.keys()) == 3
+
+
+def test_values():
+    assert next(coll.values()) == 3
+
+
+@pytest.mark.skip
+def test_values_err():
+    assert next(coll.values()) == "test"
+
+
+def test_for_each():
+    def fn(k, v, c):
+        assert k == "test"
+        assert v == 3
+        assert c == coll
+    coll.for_each(fn)
+
+
+@pytest.mark.skip
+def test_for_each_err():
+    def fn(k, v, c):
+        assert k == 3
+        assert v == coll
+        assert c == "test"
+    coll.for_each(fn)
